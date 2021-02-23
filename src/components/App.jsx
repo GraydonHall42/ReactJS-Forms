85a887a81ab806fe2b8f93e1e40374f2be898713
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-23 15:22:47 -0700

    finished module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 function App() {
+  const [name, setName] = React.useState("");
+  const [title, setTitle] = React.useState("");
+
+  function handleChange() {
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setTitle(name);
+
+    event.preventDefault(); //prevent refresh of page.
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{title} </h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
